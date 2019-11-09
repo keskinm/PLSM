563c13a972c9ead88984aee4c374f183b97fb2b3
keskin <keskin.mustafa.42@gmail.com>
2019-11-09 18:06:01 +0100

    Refactor pyro_plsm_inference

--- pyro_plsm_inference.py
+++ pyro_plsm_inference.py
@@ -12,8 +12,6 @@ from tqdm import tqdm
 import os
 import argparse
 
-torch.manual_seed(101)
-
 softplus = torch.nn.Softplus()
 
 # ADD: change figure size
@@ -22,7 +20,7 @@ plt.rc('figure', figsize=(12.0, 7.0))
 
 class PyroPLSMInference:
     def __init__(self, documents_number, relative_time_length, words_number, documents_length, latent_motifs_number,
-                 n_steps, lr, observations_file_path):
+                 n_steps, lr, observations_file_path, seed):
         self.documents_number = documents_number
         self.relative_time_length = relative_time_length
         self.words_number = words_number
@@ -32,6 +30,7 @@ class PyroPLSMInference:
         self.lr = lr
         self.observations_file_path = observations_file_path
         self.adjusted_documents_length = documents_length - relative_time_length + 1
+        torch.manual_seed(seed)
 
         # prior0 = 0.1*N/nd / nz / Td
         # prior1 = 0.1*N/nz / nw / ntr
@@ -75,12 +74,13 @@ class PyroPLSMInference:
         # NB: this is just the initialization
         s0 = (self.documents_number, self.latent_motifs_number, 1, self.adjusted_documents_length)
         s1 = (self.latent_motifs_number, 1, self.words_number, self.relative_time_length)
-        alpha0 = torch.ones(*s0)
-        alpha1 = torch.ones(*s1)
+        motifs_starting_times = torch.ones(*s0)
+        motifs = torch.ones(*s1)
         # CHANGE: use the fact that dirichlet can draw independant dirichlets
         # TODO: essayer "get_param"
-        z = pyro.sample("latent0", pdist.Dirichlet(concentration=alpha0.view(self.documents_number, -1)))
-        motifs = pyro.sample("latent1", pdist.Dirichlet(concentration=alpha1.view(self.documents_number, -1)))
+        z = pyro.sample("motifs_starting_times", pdist.Dirichlet(concentration=
+                                                                 motifs_starting_times.view(self.documents_number, -1)))
+        motifs = pyro.sample("motifs", pdist.Dirichlet(concentration=motifs.view(self.documents_number, -1)))
         # ADD: resize z and motifs
         z = z.reshape(*s0)
         motifs = motifs.reshape(*s1)
@@ -89,79 +89,74 @@ class PyroPLSMInference:
             pyro.sample("observe", pdist.Multinomial(-999, probs=self.p_w_ta_d(z, motifs)), obs=data)
 
     def guide(self, data):
-        qalpha0 = pyro.param("qalpha0", torch.ones(self.documents_number, self.latent_motifs_number, 1,
-                                                   self.adjusted_documents_length), constraint=constraints.positive)
+        q_motifs_starting_times = pyro.param("q_motifs_starting_times", torch.ones(self.documents_number,
+                                                                                   self.latent_motifs_number, 1,
+                                                                                   self.adjusted_documents_length),
+                                             constraint=constraints.positive)
 
-        qalpha1 = pyro.param("qalpha1", torch.ones(self.latent_motifs_number, 1, self.words_number,
-                                                   self.relative_time_length), constraint=constraints.positive)
+        q_motifs = pyro.param("q_motifs", torch.ones(self.latent_motifs_number, 1, self.words_number,
+                                                     self.relative_time_length), constraint=constraints.positive)
 
         # CHANGE: use the fact that dirichlet can draw independant dirichlets
-        pyro.sample("latent0", pdist.Dirichlet(concentration=qalpha0.view(self.documents_number, -1)))
-        pyro.sample("latent1", pdist.Dirichlet(concentration=qalpha1.view(self.latent_motifs_number, -1)))
+        pyro.sample("motifs_starting_times", pdist.Dirichlet(
+            concentration=q_motifs_starting_times.view(self.documents_number, -1)))
+
+        pyro.sample("motifs", pdist.Dirichlet(concentration=q_motifs.view(self.latent_motifs_number, -1)))
 
     def run_inference(self):
-        data = torch.tensor(self.load_data('./data/Junction1-b-s-m-plsa.tdoc', self.documents_length, self.words_number),
-                            dtype=torch.float32).view(-1)
-        data_vis = torch.tensor(self.load_data('./data/Junction1-b-s-m-plsa.tdoc', Tdo, nw), dtype=torch.float32)
+        data = torch.tensor(self.load_data('./data/Junction1-b-s-m-plsa.tdoc', self.documents_length,
+                                           self.words_number), dtype=torch.float32).view(-1)
 
         pyro.clear_param_store()
 
-        # CHANGE: change adam params
-        adam_params = {"lr": 0.1}
-        #adam_params = {"lr": 0.005, "betas": (0.9, 0.999)}
+        adam_params = {"lr": 0.1, "betas": (0.9, 0.999)}
         optimizer = pyro.optim.Adam(adam_params)
-        #optimizer = pyro.optim.SGD(adam_params)
 
-        svi = SVI(model, guide, optimizer, loss=Trace_ELBO())
+        svi = SVI(self.model, self.guide, optimizer, loss=Trace_ELBO())
 
-        # CHANGE: use a nice progress bar
-        n_steps = 400
+        for _ in tqdm(range(self.n_steps)):
+            svi.step(data)
 
-        # for step in tqdm(range(n_steps)):
-        #     svi.step(data)
-        #
-        # # CHANGE: change only at the end
-        # np.save(file="./data/qalpha0", arr=pyro.param("qalpha0").detach().numpy())
-        # np.save(file="./data/qalpha1", arr=pyro.param("qalpha1").detach().numpy())
+        # CHANGE: change only at the end
+        np.save(file="./data/motifs_starting_times", arr=pyro.param("q_motifs_starting_times").detach().numpy())
+        np.save(file="./data/motifs", arr=pyro.param("q_motifs").detach().numpy())
 
+        self.plots()
 
+    def plots(self):
+        # ADD: quick plot before exhaustive plot
+        motifs_starting_times_file_path = "./data/motifs_starting_times.npy"
+        motifs_file_path = "./data/motifs.npy"
 
-def plots():
-    # ADD: quick plot before exhaustive plot
-    file0 = "./data/qalpha0.npy"
-    file1 = "./data/qalpha1.npy"
+        loaded = np.load(motifs_file_path)
+        plt.imshow(-loaded.reshape(-1, self.relative_time_length), cmap="gray")
+        plt.show()
 
-    loaded = np.load(file1)
-    plt.imshow(-loaded.reshape(-1, ntr), cmap="gray")
-    plt.show()
-    for i in range(nz):
-        # plt.imshow(loaded[i].squeeze())
-        print(loaded[i].sum())
-        # plt.show()
-    loaded.sum(axis=3).shape
-    loaded.sum(axis=3) / loaded.sum(axis=3).sum(axis=2, keepdims=True)
-    plt.imshow(loaded[0].squeeze())
+        for i in range(self.latent_motifs_number):
+            plt.imshow(loaded[i].squeeze())
+            print(loaded[i].sum())
+            plt.show()
 
-    np.load(file1).sum(axis=3).sum()
-    loaded = np.load(file1)
-    pwz = loaded.sum(axis=3)
-    pwz /= pwz.sum(axis=2, keepdims=True)
-    np.savetxt('./data/results.pwz', pwz.squeeze().transpose())
-    loaded /= loaded.sum(axis=3, keepdims=True)
-    np.savetxt('./data/results.ptrwz', np.stack([loaded[i, 0].transpose() for i in range(nz)]).reshape(-1, nw))
-    loaded.shape
-    loaded.sum(axis=3)
+        plt.imshow(loaded[0].squeeze())
+        plt.show()
 
-    loaded = np.load(file0)
-    pzd = loaded.sum(axis=3)
-    pzd /= pzd.sum(axis=1, keepdims=True)
-    np.savetxt('./data/results.pzd', pzd.squeeze().transpose())
-    loaded /= loaded.sum(axis=3, keepdims=True)
-    np.savetxt('./data/results.ptszd', np.stack([loaded[i, :, 0].transpose() for i in range(nd)]).reshape(-1, nz))
-    loaded.shape
+        loaded = np.load(motifs_starting_times_file_path)
+        pzd = loaded.sum(axis=3)
+        pzd /= pzd.sum(axis=1, keepdims=True)
+        np.savetxt('./data/results.pzd', pzd.squeeze().transpose())
+        loaded /= loaded.sum(axis=3, keepdims=True)
+        np.savetxt('./data/results.ptszd', np.stack([loaded[i, :, 0].transpose() for i in range(self.documents_number)
+                                                     ]).reshape(-1, self.latent_motifs_number))
 
-
-plots()
+        np.load(motifs_file_path).sum(axis=3).sum()
+        loaded = np.load(motifs_file_path)
+        pwz = loaded.sum(axis=3)
+        pwz /= pwz.sum(axis=2, keepdims=True)
+        np.savetxt('./data/results.pwz', pwz.squeeze().transpose())
+        loaded /= loaded.sum(axis=3, keepdims=True)
+        np.savetxt('./data/results.ptrwz', np.stack([loaded[i, 0].transpose() for i in range(self.latent_motifs_number)
+                                                     ]).reshape(-1, self.words_number))
+        loaded.sum(axis=3)
 
 
 def main():
@@ -175,7 +170,6 @@ def main():
         help='Number of documents')
 
     parser.add_argument(
-        '-ntr',
         '--relative-time-length',
         type=int,
         default=20,
@@ -183,20 +177,17 @@ def main():
     )
 
     parser.add_argument(
-        '-nz'
         '--words-number',
         type=int,
         default=100,
         help='number of words')
 
     parser.add_argument(
-        '-Td'
         '--documents-length',
         action='store_true',
         help='Length of documents')
 
     parser.add_argument(
-        '-nz',
         '--latent-motifs-number',
         type=int,
         default=5,
@@ -225,7 +216,13 @@ def main():
     )
 
     parser.add_argument(
-        '--seed', type=int, default=42, help='seed of random generator')
+        '--seed', type=int, default=101, help='seed of random generator')
+
+    args = parser.parse_args()
+    args = vars(args)
+
+    pyro_plsm_inference = PyroPLSMInference(**args)
+    pyro_plsm_inference.run_inference()
 
     return parser
 
