daf750adf3b80402ef45e85179048384b4303bc7
keskin <keskin.mustafa.42@gmail.com>
2019-11-10 10:23:01 +0100

    Refactor pyro_plsm_inference

--- pyro_plsm_inference.py
+++ pyro_plsm_inference.py
@@ -12,7 +12,6 @@ from tqdm import tqdm
 import os
 import argparse
 
-softplus = torch.nn.Softplus()
 
 # ADD: change figure size
 plt.rc('figure', figsize=(12.0, 7.0))
@@ -20,7 +19,7 @@ plt.rc('figure', figsize=(12.0, 7.0))
 
 class PyroPLSMInference:
     def __init__(self, documents_number, relative_time_length, words_number, documents_length, latent_motifs_number,
-                 n_steps, lr, observations_file_path, seed):
+                 n_steps, lr, observations_file_path, work_dir, seed, plot_results):
         self.documents_number = documents_number
         self.relative_time_length = relative_time_length
         self.words_number = words_number
@@ -30,6 +29,9 @@ class PyroPLSMInference:
         self.lr = lr
         self.observations_file_path = observations_file_path
         self.adjusted_documents_length = documents_length - relative_time_length + 1
+        self.work_dir = work_dir
+        self.plot_results = plot_results if plot_results is not None else None
+        os.makedirs(work_dir, exist_ok=True)
         torch.manual_seed(seed)
 
         # prior0 = 0.1*N/nd / nz / Td
@@ -64,29 +66,36 @@ class PyroPLSMInference:
         return np.array(matrix)  # Return the Matrix
 
     @staticmethod
-    def p_w_ta_d(z, motifs):
-        t = F.conv_transpose2d(z, motifs)
+    def p_w_ta_d(motifs_starting_times, motifs):
+        t = F.conv_transpose2d(motifs_starting_times, motifs)
         # CHANGE: use shape (-1) to auto-infer
         return t.view(-1)
 
     def model(self, data):
         # ADD: factor out the shapes
         # NB: this is just the initialization
-        s0 = (self.documents_number, self.latent_motifs_number, 1, self.adjusted_documents_length)
-        s1 = (self.latent_motifs_number, 1, self.words_number, self.relative_time_length)
-        motifs_starting_times = torch.ones(*s0)
-        motifs = torch.ones(*s1)
+        motifs_starting_times_shape = (self.documents_number, self.latent_motifs_number, 1,
+                                       self.adjusted_documents_length)
+
+        motifs_shape = (self.latent_motifs_number, 1, self.words_number, self.relative_time_length)
+        motifs_starting_times_concentration = torch.ones(*motifs_starting_times_shape)
+        motifs_concentration = torch.ones(*motifs_shape)
+
         # CHANGE: use the fact that dirichlet can draw independant dirichlets
         # TODO: essayer "get_param"
-        z = pyro.sample("motifs_starting_times", pdist.Dirichlet(concentration=
-                                                                 motifs_starting_times.view(self.documents_number, -1)))
-        motifs = pyro.sample("motifs", pdist.Dirichlet(concentration=motifs.view(self.documents_number, -1)))
-        # ADD: resize z and motifs
-        z = z.reshape(*s0)
-        motifs = motifs.reshape(*s1)
+        motifs_starting_times = pyro.sample("motifs_starting_times", pdist.Dirichlet(
+            concentration=motifs_starting_times_concentration.view(self.documents_number, -1)))
+        motifs = pyro.sample("motifs", pdist.Dirichlet(
+            concentration=motifs_concentration.view(self.documents_number, -1)))
+
+        # ADD: resize motifs_starting_times and motifs
+        motifs_starting_times = motifs_starting_times.reshape(*motifs_starting_times_shape)
+        motifs = motifs.reshape(*motifs_shape)
+
         with pyro.plate("data", 360200, subsample_size=100):
             # CHANGE: make explicit the fact that the number of observation is unused here
-            pyro.sample("observe", pdist.Multinomial(-999, probs=self.p_w_ta_d(z, motifs)), obs=data)
+            pyro.sample("observe", pdist.Multinomial(-999, probs=self.p_w_ta_d(motifs_starting_times, motifs)),
+                        obs=data)
 
     def guide(self, data):
         q_motifs_starting_times = pyro.param("q_motifs_starting_times", torch.ones(self.documents_number,
@@ -104,8 +113,8 @@ class PyroPLSMInference:
         pyro.sample("motifs", pdist.Dirichlet(concentration=q_motifs.view(self.latent_motifs_number, -1)))
 
     def run_inference(self):
-        data = torch.tensor(self.load_data('./data/Junction1-b-s-m-plsa.tdoc', self.documents_length,
-                                           self.words_number), dtype=torch.float32).view(-1)
+        data = torch.tensor(self.parse_tdoc_file(self.observations_file_path, self.documents_length,
+                                                 self.words_number), dtype=torch.float32).view(-1)
 
         pyro.clear_param_store()
 
@@ -117,53 +126,57 @@ class PyroPLSMInference:
         for _ in tqdm(range(self.n_steps)):
             svi.step(data)
 
-        # CHANGE: change only at the end
-        np.save(file="./data/motifs_starting_times", arr=pyro.param("q_motifs_starting_times").detach().numpy())
-        np.save(file="./data/motifs", arr=pyro.param("q_motifs").detach().numpy())
+        motifs_starting_times_file_path = os.path.join(self.work_dir, 'motifs_starting_times.npy')
+        motifs_file_path = os.path.join(self.work_dir, 'motifs.npy')
 
-        self.plots()
+        motifs_starting_times = pyro.param("q_motifs_starting_times").detach().numpy()
+        motifs = pyro.param("q_motifs").detach().numpy()
 
-    def plots(self):
+        np.save(file=motifs_starting_times_file_path, arr=motifs_starting_times)
+        np.save(file=motifs_file_path, arr=motifs)
+
+        self.dump_motifs_and_starting_times()
+
+        if self.plot_results:
+            self.plot_motifs_and_starting_times(motifs)
+
+    def plot_motifs_and_starting_times(self, motifs):
+        plt.imshow(-motifs.reshape(-1, self.relative_time_length), cmap="gray")
+        plt.show()
+
+        for motif in motifs:
+            plt.imshow(motif.squeeze())
+            print(motif.sum())
+            plt.show()
+
+    def dump_motifs_and_starting_times(self):
         # ADD: quick plot before exhaustive plot
         motifs_starting_times_file_path = "./data/motifs_starting_times.npy"
         motifs_file_path = "./data/motifs.npy"
 
-        loaded = np.load(motifs_file_path)
-        plt.imshow(-loaded.reshape(-1, self.relative_time_length), cmap="gray")
-        plt.show()
-
-        for i in range(self.latent_motifs_number):
-            plt.imshow(loaded[i].squeeze())
-            print(loaded[i].sum())
-            plt.show()
-
-        plt.imshow(loaded[0].squeeze())
-        plt.show()
-
-        loaded = np.load(motifs_starting_times_file_path)
-        pzd = loaded.sum(axis=3)
+        motifs_starting_times = np.load(motifs_starting_times_file_path)
+        pzd = motifs_starting_times.sum(axis=3)
         pzd /= pzd.sum(axis=1, keepdims=True)
         np.savetxt('./data/results.pzd', pzd.squeeze().transpose())
-        loaded /= loaded.sum(axis=3, keepdims=True)
-        np.savetxt('./data/results.ptszd', np.stack([loaded[i, :, 0].transpose() for i in range(self.documents_number)
+        motifs_starting_times /= motifs_starting_times.sum(axis=3, keepdims=True)
+        np.savetxt('./data/results.ptszd', np.stack([motifs_starting_times[i, :, 0].transpose() for i in range(self.documents_number)
                                                      ]).reshape(-1, self.latent_motifs_number))
 
         np.load(motifs_file_path).sum(axis=3).sum()
-        loaded = np.load(motifs_file_path)
-        pwz = loaded.sum(axis=3)
+        motifs = np.load(motifs_file_path)
+        pwz = motifs.sum(axis=3)
         pwz /= pwz.sum(axis=2, keepdims=True)
         np.savetxt('./data/results.pwz', pwz.squeeze().transpose())
-        loaded /= loaded.sum(axis=3, keepdims=True)
-        np.savetxt('./data/results.ptrwz', np.stack([loaded[i, 0].transpose() for i in range(self.latent_motifs_number)
+        motifs /= motifs.sum(axis=3, keepdims=True)
+        np.savetxt('./data/results.ptrwz', np.stack([motifs[i, 0].transpose() for i in range(self.latent_motifs_number)
                                                      ]).reshape(-1, self.words_number))
-        loaded.sum(axis=3)
+        motifs.sum(axis=3)
 
 
 def main():
     parser = argparse.ArgumentParser(prog='pyro_inference')
 
     parser.add_argument(
-        '-nd',
         '--documents-number',
         type=int,
         default=1,
@@ -184,7 +197,8 @@ def main():
 
     parser.add_argument(
         '--documents-length',
-        action='store_true',
+        type=int,
+        default=3602,
         help='Length of documents')
 
     parser.add_argument(
@@ -216,7 +230,18 @@ def main():
     )
 
     parser.add_argument(
+        '--work-dir',
+        type=str,
+        default='./data/inference',
+        help=
+        'path to the working dir where results are stored'
+    )
+
+    parser.add_argument(
         '--seed', type=int, default=101, help='seed of random generator')
+
+    parser.add_argument(
+        '--plot-results', action='store_true', help='plot motifs and their starting times')
 
     args = parser.parse_args()
     args = vars(args)
