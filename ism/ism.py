9b55123965c13273a832808c5c86fdc7cb63da37
keskin <keskin.mustafa.42@gmail.com>
2019-11-17 21:03:20 +0100

    Create utils

--- ism/ism.py
+++ ism/ism.py
@@ -1,26 +1,5 @@
 import numpy as np
 
-def format_seq_file(seq_file_path):
-    seq_file = open(seq_file_path, "r")
-    contents = seq_file.read()
-    seq_file.close()
-
-    contents = contents.split(',')
-
-    new_contents = []
-
-    for content in contents:
-        stop_idx = content.find(']')
-        new_contents.append(content[:stop_idx + 1])
-
-    filtered_contents = list(filter(lambda x: len(x) != 0, new_contents))
-
-    filtered_contents = [int(content[1:-1]) for content in filtered_contents]
-
-    filtered_contents = [filtered_contents]
-
-    return filtered_contents
-
 
 class IsmHandler:
     def __init__(self, documents_number, relative_time_length, words_number, documents_length, latent_motifs_number,
