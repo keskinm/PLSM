5f33f5c276cbc3f431fc5587f756d7f33591461f
keskin <keskin.mustafa.42@gmail.com>
2019-11-17 20:52:43 +0100

    Refract tdoc parser

--- pyro_plsm_inference.py
+++ pyro_plsm_inference.py
@@ -11,6 +11,7 @@ import numpy as np
 from tqdm import tqdm
 import os
 import argparse
+from utils.parser import parse_tdoc_file
 
 
 # ADD: change figure size
@@ -45,33 +46,6 @@ class PyroPLSMInference:
         # prior0 = 0.1*N/nd / nz / Td
         # prior1 = 0.1*N/nz / nw / ntr
         # randinit = 0
-
-    @staticmethod
-    def parse_tdoc_file(filetdoc, document_length, nw):
-        matrix = [[0 for x in range(document_length)] for x in range(nw)]
-        # np.zeros((documentLength, W))  # Initialize Matrix of length 300x25 with Zeros   ,dtype=np.uint8
-
-        Dict = {}  # Define Dictionary , Index of line position : List (that contain the Values)
-        # Read the file tdoc
-        infile = open(filetdoc, 'r')  # Read the tdoc file
-        i = 0
-        for data in infile:
-            if len(data.strip()) != 0:  # take not empty line in tdoc and make some processing on Data
-                Dict[i] = [data]  # Example Dict[113] = ['12:2 13:3 14:4 15:2 16:1 \n']
-            i += 1
-        infile.close()
-        for j in Dict.keys():
-            x = Dict[j]  # x is the Value in the Dict at Index j
-            y = x[0].split(' ')  # Split at the space to get something like this ['17:1', '\n']
-            for k in range(len(y)):
-                z = y[k].split(':')  # split at : so we get ['17:1']['\n']
-                for m in range(len(z) - 1):
-                    b = int(z[m])  # b is the position of Word W and j is the time , For Example b=17
-                    q = float(z[m + 1])  # q is the value should be filled in the matrix , For example q=1 (at position j,b)
-                    matrix[b][j] = q  # Fill the Matrix with v
-    #                 matrix= np.transpose(matrix)         # To put the Matrix in the correct form
-
-        return np.array(matrix)  # Return the Matrix
 
     @staticmethod
     def p_w_ta_d(motifs_starting_times, motifs):
@@ -123,7 +97,7 @@ class PyroPLSMInference:
         pyro.sample("motifs", pdist.Dirichlet(concentration=q_motifs.view(self.latent_motifs_number, -1)))
 
     def run_inference(self):
-        data = torch.tensor(self.parse_tdoc_file(self.observations_file_path, self.documents_length,
+        data = torch.tensor(parse_tdoc_file(self.observations_file_path, self.documents_length,
                                                  self.words_number), dtype=torch.float32).view(-1)
 
         seq = self.format_seq('./mutu_data/seq.txt')
---
+++ utils/parser.py
@@ -0,0 +1,25 @@
+def parse_tdoc_file(filetdoc, document_length, nw):
+    matrix = [[0 for x in range(document_length)] for x in range(nw)]
+    # np.zeros((documentLength, W))  # Initialize Matrix of length 300x25 with Zeros   ,dtype=np.uint8
+
+    Dict = {}  # Define Dictionary , Index of line position : List (that contain the Values)
+    # Read the file tdoc
+    infile = open(filetdoc, 'r')  # Read the tdoc file
+    i = 0
+    for data in infile:
+        if len(data.strip()) != 0:  # take not empty line in tdoc and make some processing on Data
+            Dict[i] = [data]  # Example Dict[113] = ['12:2 13:3 14:4 15:2 16:1 \n']
+        i += 1
+    infile.close()
+    for j in Dict.keys():
+        x = Dict[j]  # x is the Value in the Dict at Index j
+        y = x[0].split(' ')  # Split at the space to get something like this ['17:1', '\n']
+        for k in range(len(y)):
+            z = y[k].split(':')  # split at : so we get ['17:1']['\n']
+            for m in range(len(z) - 1):
+                b = int(z[m])  # b is the position of Word W and j is the time , For Example b=17
+                q = float(z[m + 1])  # q is the value should be filled in the matrix , For example q=1 (at position j,b)
+                matrix[b][j] = q  # Fill the Matrix with v
+    #                 matrix= np.transpose(matrix)         # To put the Matrix in the correct form
+
+    return np.array(matrix)  # Return the Matrix
